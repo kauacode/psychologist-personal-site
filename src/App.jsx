718db55e0f0464ab8db2be64caa14779b10d23b2
Kaua Henrique <kauabraccio04@gmail.com>
2025-02-18 14:17:08 -0300

    feat: add Hero section with image and text

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import './App.module.css'
 import './global.css'
 import { Header } from './components/Header'
+import { Hero } from './components/Hero'
 
 export function App() {
   return (
     <div>
       <Header />
-
+      <main>
+        <Hero />
+      </main>
     </div>
   )
 }
---
+++ src/components/Hero.jsx
@@ -0,0 +1,19 @@
+import styles from './Hero.module.css';
+import fotoPerfil from '../assets/fotoPerfil.jpg'
+
+export function Hero() {
+  return (
+    <main>
+      <div className={styles.heroContainer}>
+        <div className={styles.imageContainer}>
+          <img src={fotoPerfil} alt="" />
+        </div>
+        <div className={styles.contentContainer}>
+          <h1>É possível trilhar um caminho seguro rumo ao equilíbrio emocional, garantindo mais qualidade de vida e menos ansiedade.</h1>
+          <p>A busca pelo equilíbrio emocional pode ser desafiadora, mas você não precisa percorrer esse caminho sozinho. A psicoterapia oferece um espaço acolhedor para compreender seus sentimentos, superar dificuldades e desenvolver estratégias para lidar com a ansiedade e outras questões que afetam sua qualidade de vida.
+            Com o apoio de um profissional, você poderá fortalecer sua autoconfiança, melhorar seus relacionamentos e aprender a viver de forma mais leve e saudável.  </p>
+        </div>
+      </div>
+    </main>
+  )
+}

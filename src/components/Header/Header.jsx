2e61e812ae376456aeb17ed5c660c2f2758bf5a3
Kaua Henrique <kauabraccio04@gmail.com>
2025-02-21 16:10:49 -0300

    fix: header adjusted to the correct size

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -18,24 +18,26 @@ export function Header() {
   return (
     <header className={styles.header}>
       <div className={styles.desktopOnly}>
-        <nav>
-          <img id={styles.logo} src={logo} alt="" />
-          <ul className={styles.navLinks}>
-            <li><a href="#">Início</a></li>
-            <li><a href="#">Benefícios</a></li>
-            <li><a href="#">Sobre</a></li>
-            <li><a href="#">Depoimentos</a></li>
-            <li><a href="#">Contato</a></li>
-          </ul>
-          <div className={styles.socialIcons}>
-            <a href="https://www.instagram.com/psi.patriciabraccio" target="_blank" rel="noopener noreferrer">
-              <img src={instagram} alt="Instagram" />
-            </a>
-            <a href="https://api.whatsapp.com/send/?phone=5519996438363&text=Oie%2C+gostaria+de+saber+mais+informa%C3%A7%C3%B5es+sobre+a+terapia%21+&type=phone_number&app_absent=0" target="_blank" rel="noopener noreferrer">
-              <img src={whatsapp} alt="Whatsapp" />
-            </a>
-          </div>
-        </nav>
+        <div className={styles.headerContainer}>
+          <nav>
+            <img id={styles.logo} src={logo} alt="" />
+            <ul className={styles.navLinks}>
+              <li><a href="#">Início</a></li>
+              <li><a href="#">Benefícios</a></li>
+              <li><a href="#">Sobre</a></li>
+              <li><a href="#">Depoimentos</a></li>
+              <li><a href="#">Contato</a></li>
+            </ul>
+            <div className={styles.socialIcons}>
+              <a href="https://www.instagram.com/psi.patriciabraccio" target="_blank" rel="noopener noreferrer">
+                <img src={instagram} alt="Instagram" />
+              </a>
+              <a href="https://api.whatsapp.com/send/?phone=5519996438363&text=Oie%2C+gostaria+de+saber+mais+informa%C3%A7%C3%B5es+sobre+a+terapia%21+&type=phone_number&app_absent=0" target="_blank" rel="noopener noreferrer">
+                <img src={whatsapp} alt="Whatsapp" />
+              </a>
+            </div>
+          </nav>
+        </div>
       </div>
 
       <div className={styles.headerMobile}>

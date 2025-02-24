29ac77d8baf2c0683388ef88571a7903a148641e
Kaua Henrique <kauabraccio04@gmail.com>
2025-02-24 17:39:23 -0300

    feat: add Card component with title and description

---
+++ src/components/Card/Card.jsx
@@ -0,0 +1,12 @@
+import styles from './Card.module.css'
+
+const Card = ({title, description}) => {
+  return (
+    <div className={styles.card}>
+      <h3>{title}</h3>
+      <p>{description}</p>
+    </div>
+  )
+}
+
+export default Card;
--- src/components/Wellness/Wellness.jsx
+++ src/components/Wellness/Wellness.jsx
@@ -1,10 +1,25 @@
 import styles from './Wellness.module.css'
+import Card from '../Card/Card'
 
 export function Wellness() {
+  const problems = [
+    { title: "Ansiedade", description: "Descricao ansiedade" },
+    { title: "Depressão", description: "Descricao depressão" },
+    { title: "Baixa auto-estima", description: "Descricao Baixa auto-estima" },
+    { title: "Tristeza", description: "Descricao tristeza" },
+    { title: "Fobia social", description: "Descricao Fobia social" },
+    { title: "Medo", description: "Descricao Medo" }
+  ]
+
   return (
     <main>
-      <div className={styles.background}>
-
+      <div className={styles.backgroundContainer}>
+        <h2>Talvez você esteja sentindo</h2>
+        <div className={styles.cardContainer}>
+          {problems.map((item, index) =>
+            <Card key={index} title={item.title} description={item.description} />
+          )}
+        </div>
       </div>
     </main>
   )

30111c6f2d6e70ef5b577b8fdd65acccbba4f082
Kaua Henrique <kauabraccio04@gmail.com>
2025-02-24 15:35:02 -0300

    refactor: restructure main and heroContainer, adjust backgroundWrapper for consistency

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -11,23 +11,24 @@ export function Hero() {
       setIsLoaded(true)
     }, 500);
   }, []);
-  console.log("Classes aplicadas:", isLoaded ? styles.pageLoaded : "");
 
   return (
     <main>
-      <div className={`${styles.heroContainer} ${isLoaded ? styles.pageLoaded : ""}`}>
-        <div className={styles.imageContainer}>
-          <img src={fotoPerfil} alt="" />
-        </div>
-        <div className={styles.contentContainer}>
-          <div className={styles.textContainer}>
-            <h1>É possível alcançar equilíbrio emocional, promovendo qualidade de vida e reduzindo a ansiedade.</h1>
-            <p>A busca pelo equilíbrio emocional pode ser desafiadora, mas você não precisa percorrer esse caminho sozinho. A psicoterapia oferece um espaço acolhedor para compreender seus sentimentos, superar dificuldades e desenvolver estratégias para lidar com a ansiedade e outras questões que afetam sua qualidade de vida.
-              Com o apoio de um profissional, você poderá fortalecer sua autoconfiança, melhorar seus relacionamentos e aprender a viver de forma mais leve e saudável.  </p>
+      <div className={styles.backgroundWrapper}>
+        <div className={`${styles.heroContainer} ${isLoaded ? styles.pageLoaded : ""}`}>
+          <div className={styles.imageContainer}>
+            <img src={fotoPerfil} alt="Foto de perfil" />
           </div>
-          <a className={styles.contactButton} href="https://api.whatsapp.com/send/?phone=5519996438363&text=Oie%2C+gostaria+de+saber+mais+informa%C3%A7%C3%B5es+sobre+a+terapia%21+&type=phone_number&app_absent=0">
-            Entre em contato!
-          </a>
+          <div className={styles.contentContainer}>
+            <div className={styles.textContainer}>
+              <h1>É possível alcançar equilíbrio emocional, promovendo qualidade de vida e reduzindo a ansiedade.</h1>
+              <p>A busca pelo equilíbrio emocional pode ser desafiadora, mas você não precisa percorrer esse caminho sozinho. A psicoterapia oferece um espaço acolhedor para compreender seus sentimentos, superar dificuldades e desenvolver estratégias para lidar com a ansiedade e outras questões que afetam sua qualidade de vida.
+                Com o apoio de um profissional, você poderá fortalecer sua autoconfiança, melhorar seus relacionamentos e aprender a viver de forma mais leve e saudável.</p>
+            </div>
+            <a className={styles.contactButton} href="https://api.whatsapp.com/send/?phone=5519996438363&text=Oie%2C+gostaria+de+saber+mais+informa%C3%A7%C3%B5es+sobre+a+terapia%21+&type=phone_number&app_absent=0">
+              Entre em contato!
+            </a>
+          </div>
         </div>
       </div>
     </main>
